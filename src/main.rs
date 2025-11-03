8c8491f892a3f42c23e721b9dd050b8f202800be
Mohamed A. Salah <me@msalah.net>
2025-11-03 03:03:01 +0200

    handle stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
-#![allow(unused_imports)]
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
-const CORRELATION_ID: u8 = 7u8;
 fn main() -> anyhow::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // TODO: Uncomment the code below to pass the first stage
-
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
 
     for stream in listener.incoming() {
+        let mut buffer = [0u8; 1024];
         match stream {
             Ok(mut stream) => {
-                stream.write(&[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07])?;
                 println!("accepted new connection");
+                let bytes_read = stream.read(&mut buffer)?;
+                // ensure that the size of the request at least 10 bytes long
+                assert!(bytes_read >= 12);
+                let message = &buffer[0..bytes_read];
+                let _message_size = &message[0..4];
+                let _request_api_key = &message[4..6];
+                let _request_api_version = &message[6..8];
+                let correlation_id = &message[8..12];
+
+                let mut response: Vec<u8> = vec![0x00, 0x00, 0x00, 0x00];
+                response.extend_from_slice(correlation_id);
+
+                println!("{:#?}", response);
+                stream.write_all(&response)?;
             }
             Err(e) => {
                 println!("error: {}", e);
