2ea908f34f58352d7fb36fae00541d5e7f700b2e
Mohamed A. Salah <me@msalah.net>
2025-11-09 02:45:45 +0200

    feat: write the api version and api key

--- src/main.rs
+++ src/main.rs
@@ -19,17 +19,47 @@ fn main() -> anyhow::Result<()> {
                 assert!(bytes_read >= 12);
                 let message = &buffer[0..bytes_read];
                 let _message_size = &message[0..4];
-                let _request_api_key = &message[4..6];
+                let request_api_key = &message[4..6];
                 let request_api_version = u16::from_be_bytes(message[6..8].try_into()?);
 
                 let correlation_id = &message[8..12];
 
-                let mut response: Vec<u8> = vec![0x00, 0x00, 0x00, 0x00];
+                let mut response: Vec<u8> = vec![];
+
+                // message size
+                response.extend_from_slice(&19u32.to_be_bytes());
+
+                // correlation id
                 response.extend_from_slice(correlation_id);
 
                 if request_api_version > 4 {
                     response.extend_from_slice(&[0x00, 0x23]);
                 }
+
+                // wirte the error 0
+                response.extend_from_slice(&[0, 0]);
+
+                // api keys
+                response.extend_from_slice(&[0x02]);
+
+                // api key
+                response.extend_from_slice(request_api_key);
+
+                // min version
+                response.extend_from_slice(&[0, 0]);
+
+                // max version
+                response.extend_from_slice(&[0, 4]);
+
+                // TAG_BUFFER
+                response.extend_from_slice(&[0]);
+
+                // throttle_time_ms
+                response.extend_from_slice(&[0, 0, 0, 0]);
+
+                // TAG_BUFFER
+                response.extend_from_slice(&[0]);
+
                 println!("{:#?}", response);
                 stream.write_all(&response)?;
             }
